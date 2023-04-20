58ff59262a9d7ce58f18643124934fcb6d48264c
zachyking <dominik.zachar@gmail.com>
2023-04-20 11:41:49 +0100

    add basic pages for most repos

--- README.md
+++ README.md
@@ -1,23 +1,8 @@
-# Nextra Docs Template 
+# ADAO Docs
 
-This is a template for creating documentation with [Nextra](https://nextra.site).
+This is a respository for ADAO's documentation. It is built using [Nextra](https://nextra.site) on top of next.js.
 
-[**Live Demo →**](https://nextra-docs-template.vercel.app)
+## Contributing
 
-[![](.github/screenshot.png)](https://nextra-docs-template.vercel.app)
-
-## Quick Start
-
-Click the button to clone this repository and deploy it on Vercel:
-
-[![](https://vercel.com/button)](https://vercel.com/new/clone?s=https%3A%2F%2Fgithub.com%2Fshuding%2Fnextra-docs-template&showOptionalTeamCreation=false)
-
-## Local Development
-
-First, run `pnpm i` to install the dependencies.
-
-Then, run `pnpm dev` to start the development server and visit localhost:3000.
-
-## License
-
-This project is licensed under the MIT License.
+To create new pages, create .mdx file. It's possible to use react code inside the .mdx file too, if necessary.
+When adding or updating pages, please update the sidebar config in `_meta.json`.
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Logo from './components/logo'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Logo style={{width: '3rem', height: '3rem'}}/>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/adaocommunity',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/BGuhdBXQFU',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/adaocommunity/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: "ADAO Community's documentation",
   },
+  search: {
+    loading: "Loading...",
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 0
+  },
+  head: <>
+    <link rel="icon" href="/favicon.ico" />
+    <meta name="title" content="ADAO Docs" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#fff" />
+    <meta name="description" content="ADAO Community's documentation" />
+    <meta name="og:title" content="ADAO Community's documentation" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@adaocommunity" />
+    <meta name="twitter:creator" content="@adaocommunity" />
+    <meta name="twitter:title" content="ADAO Community's documentation" />
+    <meta name="twitter:description" content="ADAO Community's documentation page" />
+    <meta name="twitter:image" content="https://www.theadao.io/wp-content/uploads/2022/02/ADAO-Full-Logo-Blue-Gradient.webp" />
+    <meta name="twitter:image:alt" content="ADAO Community's documentation" />
+  </>
 }
 
 export default config
